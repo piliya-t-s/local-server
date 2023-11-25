c46efbd48d9b25808e11f7bdf56d3cdb5289d3c3
rhgoda <135763987+piliya-t-s@users.noreply.github.com>
2023-11-25 18:40:56 +0300

    no

--- index.js
+++ index.js
@@ -1,40 +1,30 @@
 import grpc from '@grpc/grpc-js'
-import protoLoader from '@grpc/proto-loader'
 import http from 'http';
-import path from 'path';
 import RoomService from './lib/service/RoomService.js'
 import WsServerFactory from './lib/service/WsServerFactory.js';
 import configureServer from './lib/server/configureServer.js';
 import configureGRPC from './lib/server/configureGRPC.js';
+import 'dotenv/config'
 
 const WSFactory = new WsServerFactory();
 const roomService = new RoomService(WSFactory);
-
-const packageDef = protoLoader.loadSync("api.proto", {})
-const hostPackage = grpc.loadPackageDefinition(packageDef).hostApi
+const server = http.createServer();
 const grpcServer = new grpc.Server()
 
-grpcServer = configureGRPC(grpcServer, {
+configureServer(server, roomService)
+configureGRPC(grpcServer, {
     "protoFile": "api.proto", 
     "roomService": roomService
 })
 
-grpcServer.bindAsync("localhost:9000", grpc.ServerCredentials.createInsecure(), () => {
+grpcServer.bindAsync(`localhost:${process.env.GRPC_PORT}`, grpc.ServerCredentials.createInsecure(), () => {
     grpcServer.start()
 })
 
-// const server = http.createServer();
+server.listen(80, () => {
+  server.once('close', () => {
+    console.log('HTTP server closed')
+  });
+});
 
-
-
-// configureServer(server, roomService)
-// // configureExpress(app, roomService)
-
-// server.listen(80, () => {
-//   const address = server.address();
-//   console.log(address);
-
-//   server.once('close', () => {
-    
-//   });
-// });
+console.log(roomService.createRoom({"id": "1000"}))
