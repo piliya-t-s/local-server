d3b30235831d322eea46a72bf5154e00a681250c
rhgoda <135763987+piliya-t-s@users.noreply.github.com>
2023-11-25 17:50:58 +0300

    configureGRPC

--- index.js
+++ index.js
@@ -5,34 +5,27 @@ import path from 'path';
 import RoomService from './lib/service/RoomService.js'
 import WsServerFactory from './lib/service/WsServerFactory.js';
 import configureServer from './lib/server/configureServer.js';
+import configureGRPC from './lib/server/configureGRPC.js';
+
+const WSFactory = new WsServerFactory();
+const roomService = new RoomService(WSFactory);
 
 const packageDef = protoLoader.loadSync("api.proto", {})
 const hostPackage = grpc.loadPackageDefinition(packageDef).hostApi
 const grpcServer = new grpc.Server()
 
-
-
-grpcServer.addService(hostPackage.Host.service, {
-    "createRoom": createRoom,
-    "getRoom": getRoom
+grpcServer = configureGRPC(grpcServer, {
+    "protoFile": "api.proto", 
+    "roomService": roomService
 })
-
-function createRoom(call, callback) {
-    console.log(call)
-    callback(null, {})
-}
-
-function getRoom(call, callback) {
-    console.log(call)
-}
 
 grpcServer.bindAsync("localhost:9000", grpc.ServerCredentials.createInsecure(), () => {
     grpcServer.start()
 })
 
 // const server = http.createServer();
-// const WSFactory = new WsServerFactory();
-// const roomService = new RoomService(WSFactory);
+
+
 
 // configureServer(server, roomService)
 // // configureExpress(app, roomService)
