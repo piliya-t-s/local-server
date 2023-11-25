daf8c6b27bf1a9b3a1d5fecfee18cfd29c8fbce0
rhgoda <135763987+piliya-t-s@users.noreply.github.com>
2023-11-26 01:00:54 +0300

    works

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 import grpc from '@grpc/grpc-js'
+import path from 'path';
 import http from 'http';
 import RoomService from './lib/service/RoomService.js'
 import configureServer from './lib/server/configureServer.js';
-import configureGRPC from './lib/server/configureGRPC.js';
+import GRPCService from './lib/service/GRPCService.js';
 import 'dotenv/config'
 
-const roomService = new RoomService();
-const server = http.createServer();
+const roomService = new RoomService()
+const server = http.createServer()
 const grpcServer = new grpc.Server()
 
-configureServer(server, roomService)
-configureGRPC(grpcServer, {
-    "protoFile": "api.proto", 
-    "roomService": roomService
+const grpcService = new GRPCService(grpcServer, {
+    "protoFile": path.join(process.cwd(), "api.proto")
 })
+
+configureServer(server, roomService)
 
 grpcServer.bindAsync(`localhost:${process.env.GRPC_PORT}`, grpc.ServerCredentials.createInsecure(), () => {
     grpcServer.start()
