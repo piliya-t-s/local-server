ab83b504663d28aa4cd047ef0fd82880cd08a483
rhgoda <135763987+piliya-t-s@users.noreply.github.com>
2023-11-26 06:17:55 +0300

    stream

--- index.js
+++ index.js
@@ -5,37 +5,28 @@ import RoomService from './lib/service/RoomService.js'
 import GRPCController from './lib/service/GRPCController.js';
 import 'dotenv/config'
 import WebSocketRoomFactory from './lib/service/WebSocketRoomFactory.js';
-import { parseRoomId  } from './lib/server/parseUrl.js';
+import configureUpgrade from './lib/server/configureUpgrade.js';
+import configureStream from './lib/server/configureStream.js';
 
 const webSocketRoomFactory = new WebSocketRoomFactory()
 const roomService = new RoomService(webSocketRoomFactory)
 const server = http.createServer()
 const grpcServer = new grpc.Server()
 
-
 const gprcController = new GRPCController(grpcServer, roomService, {
     "protoFile": path.join(process.cwd(), "api.proto")
 })
 
+
+
+configureUpgrade(server, roomService)
+configureStream(server, roomService)
 gprcController.start(process.env.GRPC_PORT)
-
-server.on('upgrade', function upgrade(request, socket, head) {
-    let id = parseRoomId(request.url)
-
-    if (id == null) { // url did not match the room template at all
-        socket.destroy();
-        return
-    } 
-    
-    let targetRoom = null;
-    targetRoom = roomService.getRoomById(id)
-    console.log(targetRoom)
-    targetRoom.authUpgrade(request, socket, head)
-})
-
 
 server.listen(80, () => {
   server.once('close', () => {
     console.log('HTTP server closed')
   });
 });
+
+roomService.createRoom({"id":"123", "isPublic": true})
